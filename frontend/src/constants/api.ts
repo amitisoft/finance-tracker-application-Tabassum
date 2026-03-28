fa23ab889a5e6abc9cc78eac3c3db7149892c20b
Tabassum Nigar <tabassumnigar55@gmail.com>
2026-03-28 16:53:32 +0530

    Fix frontend backend host

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-  'https://finance-api-123-b2f6fffqgbf3e4dp.centralindia-01.azurewebsites.net/api';
+  'https://finance-api-123-b2f6fffxgbf3e4dp.centralindia-01.azurewebsites.net/api';
